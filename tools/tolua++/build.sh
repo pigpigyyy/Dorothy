59953df3571409e1d31cec1f1e2c543c6dd707b3
Li Jin <dragon-fly@qq.com>
2014-11-30 02:16:25 +0800

    remove platform code (for linux, marmalade, nacl, blackberry) I can not afford to maintain.

--- tools/tolua++/build.sh
+++ tools/tolua++/build.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+cd "$( dirname "${BASH_SOURCE[0]}" )"
 ./tolua++mac -t -D -L basic.lua -o ../../lua/cocos2dx_support/LuaCocos2d.cpp Cocos2d.pkg
 ./tolua++mac -t -D -L basic.lua -o ../../lua/cocos2dx_support/LuaCode.cpp LuaCode.pkg
