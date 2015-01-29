9bd41b61dfcd59f87e84ce5744285d69a4906340
Li Jin <dragon-fly@qq.com>
2015-01-30 02:09:59 +0800

    export lua api to python format for oxygen, but can`t get member field exported yet

--- tools/zbstudio/api/lua/main.lua
+++ tools/zbstudio/api/lua/main.lua
@@ -12,18 +12,12 @@ local function write(text)
 	result = result..text.."\n"
 end
 
-write("----")
-write("--- The Dorothy Module")
-write("-- @module Dorothy")
-write("local _M = {}\n")
-
 local types = {}
 for i=1,#orderedDorothy do
 	local className = orderedNames[i]
 	local class = orderedDorothy[i]
-	write("--- "..class.description:gsub("\n","\n-- "):gsub("class ",""))
-	write("-- @type "..className)
-	write("_M."..className.." = class()\n")
+	write("## @brief "..class.description:gsub("\n","\n# "))
+	write("class "..className.."("..(class.parents and class.parents[1] or "").."):\n")
 	if class.childs then
 		local apilist = {}
 		for name,item in pairs(class.childs) do
@@ -54,27 +48,31 @@ for i=1,#orderedDorothy do
 			local returns = class.childs[item].returns
 			local args = class.childs[item].args
 			local argNames = ""
-			write("--- "..class.childs[item].description:gsub("\n","\n-- "))
 			if args then
+				write("\t## @brief "..class.childs[item].description:gsub("\n","\n\t# "))
 				for nextItem in args:gsub("%s",""):gmatch("(%w+:[%w<>]+)[=%w]*") do
 					types[nextItem:match(":([%w<>]+)")] = true
 					--if default then print("default:",default) end
 					argNames = argNames..nextItem:match("(%w+):")..","
 					--print(nextItem:match("(%w+):"),nextItem:match(":([%w<>]+)"))
-					write("-- @param "..nextItem:match("(%w+):").." "..nextItem:match(":([%w<>]+)"))
+					write("\t# @param "..nextItem:match("(%w+):").." "..nextItem:match(":([%w<>]+)"))
 				end
 				argNames = argNames:sub(1,-2)
 			end
 			if returns then
 				for nextItem in returns:gsub("%s",""):gmatch("(%w+:[%w<>]+)") do
 					types[nextItem:match(":([%w<>]+)")] = true
-					write("-- @treturn "..nextItem:match(":([%w<>]+)").." "..nextItem:match("(%w+):"))
+					write("\t# @return "..nextItem:match(":([%w<>]+)").." "..nextItem:match("(%w+):"))
 				end
 			end
 			if args then
-				write("function "..className..":"..item.."("..argNames..") end\n")
+				if class.childs[item].static then
+					write("\t@classmethod")
+				end
+				write("\tdef "..item.."(self"..(argNames == "" and "" or ",")..argNames.."):\n\t\tpass\n")
 			else
-				write(className.."."..item.." = nil\n")
+				write("\t## @var "..item)
+				write("\t# "..class.childs[item].description:gsub("\n","\n\t# ").."\n")
 			end
 		end
 	end
@@ -85,7 +83,7 @@ for name,_ in pairs(types) do
 	write(name)
 end
 --]]
-local file = io.open("result.lua","w+")
+local file = io.open("Dorothy.py","w+")
 file:write(result)
 file:close()
 
