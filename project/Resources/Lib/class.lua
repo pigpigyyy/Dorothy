f39c49ae885259a759e61fccae98615be239bd85
Li Jin <dragon-fly@qq.com>
2015-11-24 12:10:55 +0800

    add accessibility check for property of lua class

--- project/Resources/Lib/class.lua
+++ project/Resources/Lib/class.lua
@@ -27,32 +27,39 @@ Inherit a C++ instance class:
 			return CCNode()
 		end,
 	})
+
+Use number index to add field to a class
+is deprecated. Example below may cause error.
+
+	MyClass = class({
+		[0] = 123
+		[1] = 998
+		[2] = 233
+	})
 ]]
 
 local function __call(self,...)
 	local inst = {}
 	setmetatable(inst,self)
-	local c_inst
 	if self.__partial then
-		c_inst = self.__partial(inst,...)
-	end
-	if c_inst then
-		local peer = tolua.getpeer(c_inst)
-		if peer then
-			for k,v in pairs(peer) do
-				inst[k] = v
+		local c_inst = self.__partial(inst,...)
+		if c_inst then
+			local peer = tolua.getpeer(c_inst)
+			if peer then
+				for k,v in pairs(peer) do
+					inst[k] = v
+				end
+				local peerClass = getmetatable(peer)
+				if peerClass then
+					local baseClass = getmetatable(self)
+					setmetatable(baseClass,peerClass) -- chaining partial class`s metatable
+				end
 			end
-			local peermt = getmetatable(peer)
-			if peermt then
-				-- chaining partial class`s metatable
-				local selfmt = getmetatable(self)
-				setmetatable(selfmt,peermt)
-			end
+			tolua.setpeer(c_inst,inst)
+			inst[0] = c_inst
 		end
-		tolua.setpeer(c_inst,inst)
-		inst[0] = c_inst
+		inst = c_inst or inst
 	end
-	inst = c_inst or inst
 	if self.__init then
 		self.__init(inst,...)
 	end
@@ -61,24 +68,24 @@ end
 
 local function __index(self,name)
 	local cls = getmetatable(self)
-	local item = cls[1][name]
+	local item = cls[1][name] -- access properties
 	if item then
 		return item(self[0] or self)
 	else
-		item = rawget(cls,name)
+		item = rawget(cls,name) -- access member functions
 		if item then
 			return item
 		else
 			local c = getmetatable(cls)
-			while c do
+			while c do -- recursive to access super classes
 				item = c[1][name]
 				if item then
-					cls[1][name] = item
+					cls[1][name] = item -- cache super properties to class
 					return item(self[0] or self)
 				else
 					item = rawget(c,name)
 					if item then
-						rawset(cls,name,item)
+						rawset(cls,name,item) -- cache super member to class
 						return item
 					end
 				end
@@ -91,22 +98,26 @@ end
 
 local function __newindex(self,name,value)
 	local cls = getmetatable(self)
-	local item = cls[2][name]
+	local item = cls[2][name] -- access properties
 	if item then
 		item(self[0] or self,value)
 	else
 		local c = getmetatable(cls)
-		while c do
+		while c do -- recursive to access super properties
 			item = c[2][name]
 			if item then
-				cls[2][name] = item
+				cls[2][name] = item -- cache super property to class
 				item(self[0] or self,value)
 				return
 			end
 			c = getmetatable(c)
 		end
-		rawset(self,name,value)
+		rawset(self,name,value) -- assign field to self
 	end
+end
+
+local function assignReadOnly()
+	error("Try to assign to a readonly property!")
 end
 
 local function class(arg1,arg2)
@@ -116,7 +127,12 @@ local function class(arg1,arg2)
 		base = arg1
 	else
 		base = {
-			{__class = function() return base end},{},
+			{
+				__class = function() return base end
+			},
+			{
+				__class = assignReadOnly
+			},
 			__index = __index,
 			__newindex = __newindex,
 			__call = __call,
@@ -127,7 +143,11 @@ local function class(arg1,arg2)
 		{
 			__class = function() return cls end,
 			__base = function() return base end,
-		},{},
+		},
+		{
+			__class = assignReadOnly,
+			__base = assignReadOnly,
+		},
 		__index = __index,
 		__newindex = __newindex,
 		__call = __call,
@@ -158,11 +178,11 @@ local function class(arg1,arg2)
 end
 
 local function property(getter,setter)
-	return {getter,setter,__fieldlevel=1}
+	return {getter,setter or assignReadOnly,__fieldlevel=1}
 end
 
 local function classfield(getter,setter)
-	return {getter,setter,__fieldlevel=0}
+	return {getter,setter or assignReadOnly,__fieldlevel=0}
 end
 
 local function classmethod(method)
