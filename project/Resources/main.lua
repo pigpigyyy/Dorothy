4aab39f3bb57e40ac2ccbcbb3e137a82db56a948
qcdong <qcdong@yeah.net>
2016-01-04 11:50:02 +0800

    move resources to res folder

--- project/Resources/main.lua
+++ project/Resources/main.lua
@@ -9,12 +9,12 @@ builtin.CCLabelTTF = function(text,fontName,fontSize)
 	if fontSize == 17
 		or fontSize == 20
 		or fontSize == 24 then
-		return CCLabelBMFont(text,"Font/Arial"..tostring(fontSize)..".fnt",CCLabelBMFont.AutomaticWidth,CCTextAlign.HCenter)
+		return CCLabelBMFont(text,"res/Font/Arial"..tostring(fontSize)..".fnt",CCLabelBMFont.AutomaticWidth,CCTextAlign.HCenter)
 	elseif fontSize == 10
 		or fontSize == 12 then
-		return CCLabelBMFont(text,"Font/Arial14.fnt",CCLabelBMFont.AutomaticWidth,CCTextAlign.HCenter)
+		return CCLabelBMFont(text,"res/Font/Arial14.fnt",CCLabelBMFont.AutomaticWidth,CCTextAlign.HCenter)
 	elseif fontSize == 14 or fontSize == 16 or fontSize == 18 then
-		return CCLabelBMFont(text,"Font/Arial17.fnt",CCLabelBMFont.AutomaticWidth,CCTextAlign.HCenter)
+		return CCLabelBMFont(text,"res/Font/Arial17.fnt",CCLabelBMFont.AutomaticWidth,CCTextAlign.HCenter)
 	end
 	return CCLabelTTF(text,fontName,fontSize)
 end
